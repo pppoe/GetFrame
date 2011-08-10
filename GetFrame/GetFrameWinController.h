47e88c33862872f39a6074687e24e897158dd7fc
mr.pppoe <mr.pppoe@gmail.com>
2011-08-10 14:52:45 +0800

    Zoom in/Out

--- GetFrame/GetFrameWinController.h
+++ GetFrame/GetFrameWinController.h
@@ -9,10 +9,21 @@
 #import <Cocoa/Cocoa.h>
 
 @interface GetFrameWinController : NSWindowController {
+
 @private
     IBOutlet NSImageView *_imageView;
+    IBOutlet NSScrollView *_scrollView;
+    
+
+    //< For Zooming    
+    NSSize _orgSize; //< Original Image Size
+    float _zoomFactor;
 }
 
 - (IBAction)openImage:(id)sender;
+
+- (IBAction)modeGetFrame:(id)sender;
+- (IBAction)zoomIn:(id)sender;
+- (IBAction)zoomOut:(id)sender;
 
 @end
--- GetFrame/GetFrameWinController.m
+++ GetFrame/GetFrameWinController.m
@@ -8,6 +8,32 @@
 
 #import "GetFrameWinController.h"
 
+/////////////////////////////////////////////////////////////////////
+//< Constants
+/////////////////////////////////////////////////////////////////////
+#define kMinWinWidth (400)
+#define kMinWinHeight (400)
+
+#define kMaxWinWidth (800)
+#define kMaxWinHeight (800)
+
+#define kImagePadding (5)
+
+#define kMaxImageWidth (5000)
+#define kMaxImageHeight (5000)
+
+#define kMinImageWidth (16)
+#define kMinImageHeight (16)
+
+#define kMinZoomFactor (1.0f)
+#define kMaxZoomFactor (8.0f)
+
+@interface GetFrameWinController (Private)
+
+- (void)adjustFrameForNewImage;
+- (void)zoomView:(NSView *)view withFactor:(float)factor;
+
+@end
 
 @implementation GetFrameWinController
 
@@ -29,6 +55,14 @@
 - (void)windowDidLoad
 {
     [super windowDidLoad];
+    
+    [[self window] setMinSize:NSMakeSize(kMinWinWidth, kMinWinHeight)];    
+    [[self window] setMaxSize:NSMakeSize(kMaxWinWidth, kMaxWinHeight)];    
+ 
+    _orgSize = [_imageView frame].size;
+    _zoomFactor = 1.0f;
+    
+    [_scrollView setDocumentView:_imageView];
 }
 
 - (IBAction)openImage:(id)sender {
@@ -46,8 +80,75 @@
         {
             [_imageView setImage:image];
             [image release];
+            
+            [self adjustFrameForNewImage];
         }
     }
+}
+
+- (IBAction)modeGetFrame:(id)sender {
+    
+}
+
+- (IBAction)zoomIn:(id)sender {
+    
+    _zoomFactor = MIN(kMaxZoomFactor, 2 * _zoomFactor);    
+    
+    [self zoomView:_imageView withFactor:_zoomFactor];
+}
+
+- (IBAction)zoomOut:(id)sender {
+    
+    _zoomFactor = MAX(kMinZoomFactor, _zoomFactor / 2.0f);    
+    
+    [self zoomView:_imageView withFactor:_zoomFactor];
+}
+
+@end
+
+@implementation GetFrameWinController (Private)
+
+- (void)zoomView:(NSView *)view withFactor:(float)factor {
+    NSSize s = [view frame].size;
+    NSPoint org = [view frame].origin;
+    NSPoint ctrPt = NSMakePoint(org.x + s.width/2.0f, org.y + s.height/2.0f);
+    NSSize newSize = NSMakeSize(_orgSize.width * factor, _orgSize.height * factor);
+    [[view animator] setFrame:NSMakeRect(ctrPt.x - newSize.width/2.0f, 
+                                         ctrPt.y - newSize.height/2.0f,
+                                         newSize.width, newSize.height)];
+}
+
+- (void)adjustFrameForNewImage 
+{
+    
+//    NSSize size = [[_imageView image] size];
+//    
+//    //< Keep the Left and Bottom Padding
+//    float leftPadding = _imageView.frame.origin.x;
+//    float bottomPadding = _imageView.frame.origin.y;
+//    
+//    //< Keep the Center Point
+//    NSSize winSize = NSMakeSize(MIN(MAX((size.width + 2 * kImagePadding + 2 * leftPadding), kMinWinWidth), kMaxWinWidth),
+//                                MIN(MAX((size.height + 2 * kImagePadding + 2 * bottomPadding), kMinWinHeight), kMaxWinHeight));
+//    NSRect oldWinFrame = [[self window] frame];
+//    NSPoint ctrPoint = NSMakePoint(oldWinFrame.origin.x + oldWinFrame.size.width/2.0f,
+//                                   oldWinFrame.origin.y + oldWinFrame.size.height/2.0f);
+//
+//    NSRect winFrame = NSMakeRect(ctrPoint.x - winSize.width/2.0f, 
+//                                 ctrPoint.y - winSize.height/2.0f, 
+//                                 winSize.width, winSize.height);
+//    [[self window] setFrame:winFrame
+//                    display:NO
+//                    animate:YES];
+//    
+//    NSRect rect = NSMakeRect((winSize.width - (size.width + 2*kImagePadding))/2.0f, 
+//                             (winSize.height - (size.height + 2*kImagePadding))/2.0f, 
+//                             (size.width + 2*kImagePadding), 
+//                             (size.height + 2*kImagePadding));
+//    [_imageView setFrame:rect];    
+//    
+//    [[self window] setMinSize:NSMakeSize(winFrame.size.width, winFrame.size.height)];
+    
 }
 
 @end
